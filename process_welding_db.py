74f775c241e49c638075c7968a9aa5194c35a06a
Rohit Choubey <rohitkr99@gmail.com>
2026-05-22 16:41:34 +0700

    Update process_welding_db.py

--- process_welding_db.py
+++ process_welding_db.py
@@ -26,8 +26,50 @@ def process_file(uploaded_file):
     else:
         raise ValueError("Unsupported file format")
 
-    # Example processing columns
-    df["Match_Status"] = ""
-    df["Remarks"] = ""
+        # Validation Columns
+    df["Validation_Status"] = "PASS"
+    df["Validation_Remarks"] = ""
+
+    # Missing Welder Check
+    welder_missing = (
+        df["Welder1"].isna()
+    )
+
+    df.loc[
+        welder_missing,
+        "Validation_Status"
+    ] = "FAIL"
+
+    df.loc[
+        welder_missing,
+        "Validation_Remarks"
+    ] += "Missing Welder1; "
+
+    # Missing WPS Check
+    wps_missing = (
+        df["WPS No."].isna()
+    )
+
+    df.loc[
+        wps_missing,
+        "Validation_Status"
+    ] = "FAIL"
+
+    df.loc[
+        wps_missing,
+        "Validation_Remarks"
+    ] += "Missing WPS No.; "
+
+    # Duplicate Barcode Check
+    duplicate_barcode = (
+        df["Barcode"].duplicated(keep=False)
+    )
+
+    df["Duplicate_Flag"] = duplicate_barcode.map(
+        {
+            True: "YES",
+            False: "NO"
+        }
+    )
 
     return df
