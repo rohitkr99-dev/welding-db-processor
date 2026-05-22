809cd8bee44befe08c745674beb6f4130b4a5c78
Rohit Choubey <rohitkr99@gmail.com>
2026-05-22 16:34:27 +0700

    Update process_welding_db.py

--- process_welding_db.py
+++ process_welding_db.py
@@ -3,12 +3,28 @@ import pandas as pd
 
 def process_file(uploaded_file):
 
-    # Read XLSB file
-    df = pd.read_excel(
-        uploaded_file,
-        sheet_name=0,
-        engine="pyxlsb"
-    )
+    file_name = uploaded_file.name.lower()
+
+    # Read XLSB
+    if file_name.endswith(".xlsb"):
+
+        df = pd.read_excel(
+            uploaded_file,
+            sheet_name=0,
+            engine="pyxlsb"
+        )
+
+    # Read XLSX
+    elif file_name.endswith(".xlsx"):
+
+        df = pd.read_excel(
+            uploaded_file,
+            sheet_name=0,
+            engine="openpyxl"
+        )
+
+    else:
+        raise ValueError("Unsupported file format")
 
     # Example processing columns
     df["Match_Status"] = ""
