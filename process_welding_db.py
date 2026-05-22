baffa89c717b3c72cca2235b0781240f2bcc36bd
Rohit Choubey <rohitkr99@gmail.com>
2026-05-22 16:44:06 +0700

    Update process_welding_db.py

--- process_welding_db.py
+++ process_welding_db.py
@@ -5,7 +5,7 @@ def process_file(uploaded_file):
 
     file_name = uploaded_file.name.lower()
 
-    # Read XLSB
+    # Read XLSB File
     if file_name.endswith(".xlsb"):
 
         df = pd.read_excel(
@@ -14,7 +14,7 @@ def process_file(uploaded_file):
             engine="pyxlsb"
         )
 
-    # Read XLSX
+    # Read XLSX File
     elif file_name.endswith(".xlsx"):
 
         df = pd.read_excel(
@@ -26,50 +26,68 @@ def process_file(uploaded_file):
     else:
         raise ValueError("Unsupported file format")
 
-        # Validation Columns
+    # Clean Column Names
+    df.columns = df.columns.str.strip()
+
+    # Create Validation Columns
     df["Validation_Status"] = "PASS"
     df["Validation_Remarks"] = ""
 
-    # Missing Welder Check
-    welder_missing = (
-        df["Welder1"].isna()
-    )
+    # -----------------------------
+    # Missing Welder1 Check
+    # -----------------------------
+    if "Welder1" in df.columns:
 
-    df.loc[
-        welder_missing,
-        "Validation_Status"
-    ] = "FAIL"
+        welder_missing = (
+            df["Welder1"].isna()
+        )
 
-    df.loc[
-        welder_missing,
-        "Validation_Remarks"
-    ] += "Missing Welder1; "
+        df.loc[
+            welder_missing,
+            "Validation_Status"
+        ] = "FAIL"
 
-    # Missing WPS Check
-    wps_missing = (
-        df["WPS No."].isna()
-    )
+        df.loc[
+            welder_missing,
+            "Validation_Remarks"
+        ] += "Missing Welder1; "
 
-    df.loc[
-        wps_missing,
-        "Validation_Status"
-    ] = "FAIL"
+    # -----------------------------
+    # Missing WPS No. Check
+    # -----------------------------
+    if "WPS No." in df.columns:
 
-    df.loc[
-        wps_missing,
-        "Validation_Remarks"
-    ] += "Missing WPS No.; "
+        wps_missing = (
+            df["WPS No."].isna()
+        )
 
+        df.loc[
+            wps_missing,
+            "Validation_Status"
+        ] = "FAIL"
+
+        df.loc[
+            wps_missing,
+            "Validation_Remarks"
+        ] += "Missing WPS No.; "
+
+    # -----------------------------
     # Duplicate Barcode Check
-    duplicate_barcode = (
-        df["Barcode"].duplicated(keep=False)
-    )
+    # -----------------------------
+    if "Barcode" in df.columns:
 
-    df["Duplicate_Flag"] = duplicate_barcode.map(
-        {
-            True: "YES",
-            False: "NO"
-        }
-    )
+        duplicate_barcode = (
+            df["Barcode"].duplicated(keep=False)
+        )
+
+        df["Duplicate_Flag"] = duplicate_barcode.map(
+            {
+                True: "YES",
+                False: "NO"
+            }
+        )
+
+    else:
+        df["Duplicate_Flag"] = "COLUMN NOT FOUND"
 
     return df
