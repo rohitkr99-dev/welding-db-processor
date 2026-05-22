a478ea42fd7f52c1d3da7a53f006da58659c0ce0
Rohit Choubey <rohitkr99@gmail.com>
2026-05-22 16:31:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+from io import BytesIO
+
+from process_welding_db import process_file
+
 
 st.title("Welding DB Processor")
 
@@ -8,5 +13,27 @@ uploaded_file = st.file_uploader(
 )
 
 if uploaded_file is not None:
+
     st.success("File uploaded successfully")
-    st.write("Filename:", uploaded_file.name)
+
+    if st.button("Process File"):
+
+        processed_df = process_file(uploaded_file)
+
+        output = BytesIO()
+
+        with pd.ExcelWriter(output, engine="openpyxl") as writer:
+            processed_df.to_excel(
+                writer,
+                index=False,
+                sheet_name="Processed_Data"
+            )
+
+        output.seek(0)
+
+        st.download_button(
+            label="Download Processed File",
+            data=output,
+            file_name="Welding_DB_Processed.xlsx",
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
